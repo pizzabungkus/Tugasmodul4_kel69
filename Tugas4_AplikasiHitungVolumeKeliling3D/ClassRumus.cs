223d80e3d196df2c666caac5339e1073bf50e2b7
Apriyandro Triandtio Kusumo <Apriyandrokusumo@gmail.com>
2020-04-24 00:03:40 +0700

    tugasmodul4_kel69(1)

---
+++ Tugas4_AplikasiHitungVolumeKeliling3D/ClassRumus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tugas4_AplikasiHitungVolumeKeliling3D
+{
+    class ClassRumus
+    {
+        public double volumeBalok(double a, double b, double c)
+        {          
+            double d = a * b * c;
+            Console.WriteLine("Volume Balok = " + d + " cm^3");
+            return d;
+        }
+        public double kelilingBalok(double a, double b, double c)
+        {
+            double d = 4*(a+b+c);
+            Console.WriteLine("Keliling Balok = " + d + " cm");
+            return d;
+        }
+        public double volumeKubus(double a)
+        {
+            double d = a * a * a; 
+            Console.WriteLine("Volume Kubus = " + d + " cm^3");
+            return d;
+        }
+        public double kelilingKubus(double a)
+        {
+            double d = 12 * a;
+            Console.WriteLine("Keliling Kubus = " + d + " cm");
+            return d;
+        }
+       
+    }
+}
---
+++ Tugas4_AplikasiHitungVolumeKeliling3D/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Tugas4_AplikasiHitungVolumeKeliling3D
+{
+    class Program
+    {
+        static void Intro()
+        {
+            Console.WriteLine("====Selamat datang di Aplikasi Perhitungan Volume dan Keliling Bangun 3D Kelompok 69==== ");
+            Console.WriteLine("====Di Aplikasi ini, kamu bisa menghitung Volume dan Keliling dari bangun 3D seperti Balok, Kubus, dan Tabung==== ");
+        }
+
+        static void Main(string[] args)
+        {
+            bool ulang = true;
+            string pilihan1, pilihan2;
+
+            Intro();
+            while (ulang == true)
+            {
+                Console.WriteLine(" ----Pilih Bangunan apa yang ingin dihitung---- ");
+                Console.WriteLine(" ----Tekan 1 Untuk Bangun Balok---- ");
+                Console.WriteLine(" ----Tekan 2 Untuk Bangun Kubus---- ");
+                Console.WriteLine(" ----Tekan 3 Untuk Bangun Tabung---- ");
+                pilihan1 = Console.ReadLine();
+                //BALOK
+                if (pilihan1 == "1")
+                {
+                    Console.WriteLine("Tekan 1 untuk mengihitung Volume Balok, Tekan 2 untuk menghitung Keliling Balok");
+                    pilihan2 = Console.ReadLine();
+                    ClassRumus objek = new ClassRumus();
+                    if (pilihan2 == "1")
+                    {
+                        Console.WriteLine("Masukan Panjang Balok = (Dalam CM) ");
+                        double p = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Masukan Lebar Balok = (Dalam CM) ");
+                        double l = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Masukan Tinggi Balok = (Dalam CM) ");
+                        double t = Convert.ToDouble(Console.ReadLine());
+                        objek.volumeBalok(p, l, t);
+                        perulangan();
+                    }
+                    else if (pilihan2 == "2")
+                    {
+                        Console.WriteLine("Masukan Panjang Balok = (Dalam CM) ");
+                        double p = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Masukan Lebar Balok = (Dalam CM) ");
+                        double l = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Masukan Tinggi Balok = (Dalam CM) ");
+                        double t = Convert.ToDouble(Console.ReadLine());
+                        objek.kelilingBalok(p, l, t);
+                        perulangan();
+                    }
+                    else Console.WriteLine("Pilihan yang anda tekan salah !");
+                }
+                //KUBUS
+                else if (pilihan1 == "2")
+                {
+                    Console.WriteLine("Tekan 1 untuk mengihitung Volume Kubus, Tekan 2 untuk menghitung Keliling Kubus");
+                    pilihan2 = Console.ReadLine();
+                    ClassRumus objek = new ClassRumus();
+                    if (pilihan2 == "1")
+                    {
+                        Console.WriteLine("Masukan Rusuk Kubus = (Dalam CM) ");
+                        double r = Convert.ToDouble(Console.ReadLine());
+                        objek.volumeKubus(r);
+                        perulangan();
+                    }
+                    else if (pilihan2 == "2")
+                    {
+
+                        Console.WriteLine("Masukan Rusuk Kubus = (Dalam CM) ");
+                        double r = Convert.ToDouble(Console.ReadLine());
+                        objek.kelilingKubus(r);
+                        perulangan();
+                    }
+                    else Console.WriteLine("Pilihan yang anda tekan salah !");
+                }
+
+                void perulangan()
+                {
+                    bool ulang2 = true;
+                    while (ulang2 == true)
+                    {
+                        Console.WriteLine("Apakah anda ingin menghitung lagi dengan aplikasi ini ?, tekan y untuk Ya, n untuk selesai");
+                        string yesno;
+                        yesno = Console.ReadLine();
+                        if (yesno == "y")
+                        {
+                            ulang = true;
+                            ulang2 = false;
+                        }
+                        else if (yesno == "n")
+                        {
+                            ulang = false;
+                            ulang2 = false;
+                        }
+                        else Console.WriteLine("Mohon untuk menekan y atau n untuk melanjutkan");
+                    }
+                }
+            }
+        }
+    }
+}
