8ed364f1e177be67bfb670bdd7cc32b22556749a
Vanillatte68 <roselinyosefa@yahoo.com>
2020-04-24 10:43:37 +0700

    tambahan

--- Tugas4_AplikasiHitungVolumeKeliling3D/ClassRumus.cs
+++ Tugas4_AplikasiHitungVolumeKeliling3D/ClassRumus.cs
@@ -30,6 +30,20 @@ namespace Tugas4_AplikasiHitungVolumeKeliling3D
             Console.WriteLine("Keliling Kubus = " + d + " cm");
             return d;
         }
+        public double volumeTabung(double a, double b)
+        {
+            double phi = 3.14;
+            double d = phi * (a * a) * b;
+            Console.WriteLine("Volume Tabung = " + d + "cm^3");
+            return d;
+        }
+        public double kelilingTabung(double a)
+        {
+            double phi = 3.14;
+            double d = 2 * phi * a;
+            Console.WriteLine("Keliling Alas Tabung = " + d + "cm^3");
+            return d;
+        }
        
     }
 }
